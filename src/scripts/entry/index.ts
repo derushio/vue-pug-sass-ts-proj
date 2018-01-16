b2e1376f75c2498f1ed2e43835462527e0038742
derushio <narumi18wa@gmail.com>
2018-01-16 23:00:56 +0900

    Clean code

--- src/scripts/entry/index.ts
+++ src/scripts/entry/index.ts
@@ -1,30 +1,38 @@
 /**
- * Vues
+ * Imports
  */
 import Vue from 'vue';
 import Test from '@/components/Test.vue';
-
-/**
- * Url Utils
- */
 import UrlUtil, { Params } from '@/scripts/util/UrlUtil';
-const params: Params = UrlUtil.getUrlParams();
 
 /**
  * Require sass
  */
 require('@/styles/entry/index.sass');
+
 /**
  * Vue root
  */
 class Index extends Vue {}
 
 /**
- * Register vue component
+ * init
  */
-Vue.component('test', Test);
+async function init(): Promise<void> {
+    /**
+     * GetUrlParams
+     */
+    const params: Params = UrlUtil.getUrlParams();
 
-/**
- * Mount vue root
- */
-new Index().$mount('#index');
+    /**
+     * Register vue component
+     */
+    Vue.component('test', Test);
+
+    /**
+     * Mount vue root
+     */
+    new Index().$mount('#index');
+}
+
+init();
--- src/scripts/entry/sub.ts
+++ src/scripts/entry/sub.ts
@@ -1,30 +1,38 @@
 /**
- * Vues
+ * Imports
  */
 import Vue from 'vue';
 import Test from '@/components/Test.vue';
-
-/**
- * Url Utils
- */
 import UrlUtil, { Params } from '@/scripts/util/UrlUtil';
-const params: Params = UrlUtil.getUrlParams();
 
 /**
  * Require sass
  */
 require('@/styles/entry/sub.sass');
+
 /**
  * Vue root
  */
 class Sub extends Vue {}
 
 /**
- * Register vue component
+ * init
  */
-Vue.component('test', Test);
+async function init(): Promise<void> {
+    /**
+     * GetUrlParams
+     */
+    const params: Params = UrlUtil.getUrlParams();
 
-/**
- * Mount vue root
- */
-new Sub().$mount('#sub');
+    /**
+     * Register vue component
+     */
+    Vue.component('test', Test);
+
+    /**
+     * Mount vue root
+     */
+    new Sub().$mount('#sub');
+}
+
+init();
