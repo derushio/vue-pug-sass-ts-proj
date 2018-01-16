8e2bddd47c44a19729a4344ff83ad7b029933ac4
derushio <narumi18wa@gmail.com>
2018-01-16 21:02:08 +0900

    Fix import and add comment

--- src/scripts/entry/index.ts
+++ src/scripts/entry/index.ts
@@ -1,13 +1,30 @@
+/**
+ * Vues
+ */
 import Vue from 'vue';
-import Test from '../../components/Test.vue';
+import Test from '@/components/Test.vue';
 
-import UrlUtil, { Params } from '../../scripts/util/UrlUtil';
-
+/**
+ * Url Utils
+ */
+import UrlUtil, { Params } from '@/scripts/util/UrlUtil';
 const params: Params = UrlUtil.getUrlParams();
 
-require('../../styles/entry/index.sass');
+/**
+ * Require sass
+ */
+require('@/styles/entry/index.sass');
+/**
+ *
+ */
 class Index extends Vue {}
 
+/**
+ * Register vue component
+ */
 Vue.component('test', Test);
 
+/**
+ * Mount vue root
+ */
 new Index().$mount('#index');
