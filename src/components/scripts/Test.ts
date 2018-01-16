10ba2a28b8652994b600c355a3fc40cd90a2b600
derushio <narumi18wa@gmail.com>
2018-01-16 16:50:53 +0900

    旧分けて書いてたファイルを削除

--- src/components/scripts/Test.ts
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import Component from 'vue-class-component';
-
-require('../styles/Test.sass');
-@Component({template: require('../views/Test.pug')()})
-export default class Test extends Vue {
-    private text: string = 'Hello Vue.';
-};
