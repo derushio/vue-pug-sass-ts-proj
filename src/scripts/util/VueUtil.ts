952f471a5f13b68ec5a8a83fb966c77e85a6fa99
derushio <narumi18wa@gmail.com>
2018-02-01 15:07:31 +0900

    update vue utils

--- src/scripts/util/VueUtil.ts
+++ src/scripts/util/VueUtil.ts
@@ -8,6 +8,11 @@ import { VueConstructor } from 'vue/types/vue';
  * 使用できるUrilクラス
  */
 export default class VueUtil {
+    /**
+     * 登録済みComponents
+     */
+    private static registeredComponents: VueConstructor<Vue>[] = [];
+
     private constructor() {}
 
     /**
@@ -16,11 +21,17 @@ export default class VueUtil {
      */
     public static registerComponents(components: VueConstructor<Vue>[]) {
         components.forEach((component) => {
+            if (this.registeredComponents.includes(component)) {
+                // 登録済みは登録しない
+                return;
+            }
+
             // アッパーキャメルケースからハイフネーションに変換
             const tagName: string = component.name.replace(/([A-Z])/g, '-$1')
                 .toLocaleLowerCase().replace(/^-/, '');
             // Vueに登録
             Vue.component(tagName, component);
+            this.registeredComponents.push(component)
         });
     }
 }
