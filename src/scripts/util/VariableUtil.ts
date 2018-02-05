3407262637b510fe6807fa35bb2bc8b8bf21b01e
derushio <narumi18wa@gmail.com>
2018-02-05 17:31:53 +0900

    fix vardef

--- src/scripts/util/VariableUtil.ts
+++ src/scripts/util/VariableUtil.ts
@@ -1,9 +1,19 @@
 export default class VariableUtil {
     private constructor() {};
 
-    public static variableWithDefault<T1, T2>(variable: T1, def: T2): T1 | T2 {
+    /**
+     * 変数をデフォルト指定して取得
+     * 存在が保証されない変数を安全に取得
+     * @param variable
+     * @param key
+     * @param def
+     */
+    public static vardef<T1, T2>(parent: any, key: string, def: T2): T1 | T2 {
         try {
-            return variable || def;
+            const keys: string[] = key.split('.');
+            return keys.reduce((prev: any, k) => {
+                return prev[k];
+            }, parent) || def;
         } catch(e) {
             return def;
         }
