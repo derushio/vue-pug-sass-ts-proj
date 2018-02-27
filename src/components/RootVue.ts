08dfc8e9c9aa6a6a5fd14fab902f599320a6a0e9
derushio <narumi18wa@gmail.com>
2018-02-28 01:01:46 +0900

    [Update] abstract class

--- src/components/RootVue.ts
+++ src/components/RootVue.ts
@@ -1,9 +1,9 @@
 import { Vue } from 'vue-property-decorator';
 
 // TODO: vue-property-decoratorへ以降
-export default class RootVue extends Vue {
-    public title: string = '';
-    protected beforeCreate(): void {};
+export default abstract class RootVue extends Vue {
+    public abstract title: string;
+    protected abstract beforeCreate(): void;
 
     protected reactiveTitle(): void {
         document.title = this.title;
