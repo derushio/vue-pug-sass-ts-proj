9278e115f337ebee0697fd3af08c1b3362651149
derushio <narumi18wa@gmail.com>
2018-02-28 01:03:03 +0900

    [Add] comment

--- src/components/RootVue.ts
+++ src/components/RootVue.ts
@@ -3,6 +3,10 @@ import { Vue } from 'vue-property-decorator';
 // TODO: vue-property-decoratorへ以降
 export default abstract class RootVue extends Vue {
     public abstract title: string;
+
+    /**
+     * Inner VueのComponentsをここで登録する
+     */
     protected abstract beforeCreate(): void;
 
     protected reactiveTitle(): void {
