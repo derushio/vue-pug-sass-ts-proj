dcac873df9e0f340ef92d6fab2eacc321d7b85f1
derushio <narumi18wa@gmail.com>
2018-03-31 20:50:18 +0900

    [Add] CommonNavbar

--- src/components/common/CommonNavbar.vue
+++ src/components/common/CommonNavbar.vue
@@ -1,16 +1,74 @@
 <template lang='pug'>
-.vue-common-navbar
+header.vue-common-navbar.navbar(
+        role='navigation' aria-label='main navigation')
+    .container
+        .navbar-brand
+            a.navbar-item {{ brand }}
+
+            .navbar-burger(:class='{"is-active": showNavMenu}'
+                    @click='invertShowNavMenu')
+                span(v-for='i in 3' :key='i')
+
+        nav.navbar-menu(:class='{"is-active": showNavMenu}')
+            //- menus
+            .navbar-start
+                template(v-for='menu in menus')
+                    .navbar-item.has-dropdown.is-hoverable(v-if='menu.children != null')
+                        a.navbar-link.icon-text
+                            b-icon(v-if='menu.icon != null' :pack='menu.pack || "mdi"'
+                                :icon='menu.icon')
+                            span {{ menu.text }}
+                        .navbar-dropdown
+                            a.navbar-item.icon-text(v-for='child in menu.children')
+                                b-icon(v-if='child.icon != null' :pack='child.pack || "mdi"'
+                                    :icon='child.icon')
+                                span {{ menu.text }}
+                    a.navbar-item.icon-text(v-if='menu.children == null')
+                        b-icon(v-if='menu.icon != null' :pack='menu.pack || "mdi"'
+                                :icon='menu.icon')
+                        span {{ menu.text }}
+            //- submenus
+            .navbar-end
+                template(v-for='menu in submenus')
+                    .navbar-item.has-dropdown.is-hoverable(v-if='menu.children != null')
+                        a.navbar-link.icon-text
+                            b-icon(v-if='menu.icon != null' :pack='menu.pack || "mdi"'
+                                :icon='menu.icon')
+                            span {{ menu.text }}
+                        .navbar-dropdown
+                            a.navbar-item.icon-text(v-for='child in menu.children')
+                                b-icon(v-if='child.icon != null' :pack='child.pack || "mdi"'
+                                    :icon='child.icon')
+                                span {{ menu.text }}
+                    a.navbar-item.icon-text(v-if='menu.children == null')
+                        b-icon(v-if='menu.icon != null' :pack='menu.pack || "mdi"'
+                                :icon='menu.icon')
+                        span {{ menu.text }}
 </template>
 
 <script lang='ts'>
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component, Prop } from 'vue-property-decorator';
 import VueUtil from '@/scripts/util/VueUtil';
+
+import { NavbarMenuItem } from '@/scripts/model/part/CommonNavbar';
 
 /**
  * Vue Component
  */
 @Component
 export default class CommonNavbar extends Vue {
+    @Prop({type: String, default: () => ''})
+    protected brand?: string;
+
+    @Prop({type: Array})
+    protected menus?: NavbarMenuItem[];
+    @Prop({type: Array})
+    protected submenus?: NavbarMenuItem[];
+
+    protected showNavMenu = false;
+    protected invertShowNavMenu() {
+        this.showNavMenu = !this.showNavMenu;
+    }
 }
 </script>
 
