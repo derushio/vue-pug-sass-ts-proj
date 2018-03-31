6c49b3c546ba95c2431384be687908b794fe8a98
derushio <narumi18wa@gmail.com>
2018-03-31 18:15:48 +0900

    [Add] common navbar

---
+++ src/components/common/CommonNavbar.vue
@@ -0,0 +1,21 @@
+<template lang='pug'>
+.vue-common-navbar
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+import VueUtil from '@/scripts/util/VueUtil';
+
+/**
+ * Vue Component
+ */
+@Component
+export default class CommonNavbar extends Vue {
+}
+</script>
+
+<style lang='sass' scoped>
+@import 'variable'
+
+.vue-common-navbar
+</style>
