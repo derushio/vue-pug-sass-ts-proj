28812d114b5d8d095d4e9047c29ed27cdc8840d0
derushio <narumi18wa@gmail.com>
2018-04-24 20:35:31 +0900

    [add] template

---
+++ _template/Entry.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+.vue-$_PAGENAME_$
+    .reactive-title {{ reactiveTitle() }}
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+import VueUtil from '@/scripts/util/VueUtil';
+import RootVue from '@/components/base/RootVue';
+
+import Buefy from 'buefy';
+Vue.use(Buefy);
+
+/**
+ * Vue Component
+ */
+@Component
+export default class $_FILENAME_$ extends RootVue {
+    public title = '';
+    public subtitle = '$_FILENAME_$';
+
+    protected beforeCreate(): void {
+        // Inner Vue 登録
+        VueUtil.registerComponents([]);
+    }
+}
+</script>
+
+<style lang='sass'>
+@import 'entry/all'
+
+.vue-$_PAGENAME_$
+</style>
---
+++ _template/Vue.vue
@@ -0,0 +1,21 @@
+<template lang='pug'>
+.vue-$_PAGENAME_$
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+import VueUtil from '@/scripts/util/VueUtil';
+
+/**
+ * Vue Component
+ */
+@Component
+export default class _FILENAME_ extends Vue {
+}
+</script>
+
+<style lang='sass' scoped>
+@import 'entry/variable'
+
+.vue-$_PAGENAME_$
+</style>
