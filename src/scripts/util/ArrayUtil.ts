cd6707818755833d5ddb4bef91cbf17997bfcd7a
derushio <narumi18wa@gmail.com>
2018-05-01 16:18:33 +0900

    update arrayutils

--- src/scripts/util/ArrayUtil.ts
+++ src/scripts/util/ArrayUtil.ts
@@ -4,10 +4,18 @@
 export default class ArrayUtil {
     private constructor() {}
 
+    public static gen(num: number): number[];
+
     /**
      * Arrayを生成
      */
-    public static gen(min: number, max: number): number[] {
+    public static gen(min: number, max?: number): number[] {
+        // overload
+        if (max == null) {
+            max = min;
+            min = 1;
+        }
+
         const array = [];
         for (let i = min; i <= max; i++) {
             array.push(i);
@@ -15,4 +23,5 @@ export default class ArrayUtil {
 
         return array;
     }
+
 }
